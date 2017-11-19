a20915bdcab6f97f189d01d2f56dfcb1841fc309
Choon Kiat Lee <choonkiat.lee@gmail.com>
2017-11-19 15:45:06 +0000

    fixed refresh issue

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -13,17 +13,20 @@ consumer_key = "C8BJloQw0sLBFi1Hy4imCVTSI"
 consumer_secret = "EnfX5b4eale6gwmZ2mGnC9SVavMkan4MyzzKfaDqWMSxmKBMZI"
 
 #Collect cached twitter data
-tweets_data_path = "bitcoin.txt"
+cached_tweets_data_path = "out.txt"
 
-tweets_data = []
-tweets_file = open(tweets_data_path, "r")
-for line in tweets_file:
+cached_tweets_data = []
+cached_tweets_file = open(cached_tweets_data_path, "r")
+for line in cached_tweets_file:
     try:
-        tweet = json.loads(line)
-        tweets_data.append(tweet)
+        cached_tweet = json.loads(line)
+        cached_tweets_data.append(tweet)
     except:
         #print("failed")
         continue
+
+cached_tweets_file.close();
+
 
 favourites = []
 RT = []
@@ -33,7 +36,7 @@ created_at = []
 user_name = []
 profile_pic = []
 
-for tweet in tweets_data:
+for tweet in cached_tweets_data:
     created = tweet['created_at']
     created_at.append(created[0:created.find("+")-1])
     text.append(tweet['text'])
@@ -48,88 +51,35 @@ for tweet in tweets_data:
     else:
         RT.append(0)
     
-tweet_dict = {"created_at":created_at,"user_name":user_name,"profile_pic":profile_pic,"text":text,"favourites":favourites,"RT":RT,"followers":followers}
+cached_tweet_dict = {"created_at":created_at,"user_name":user_name,"profile_pic":profile_pic,"text":text,"favourites":favourites,"RT":RT,"followers":followers}
     
-df = pd.DataFrame(data=tweet_dict)
+df = pd.DataFrame(data=cached_tweet_dict)
+
+df.sort_values(['RT'],ascending=False)
+
+df.sort_values(['RT'],ascending=False)
+
+df['z'] = df.apply(lambda row: row.RT + row.favourites, axis=1)
+
+sorted=df.sort_values(["z"],ascending=False)
 
 
-import time
-
-#This is a basic listener that parses the json lines.
-class StdOutListener(StreamListener):
-
-    def __init__(self, time_limit=60):
-        self.start_time = time.time()
-        self.limit = 60
-
-    
-    def on_data(self, data):
-        if (time.time() - self.start_time) < self.limit:
-            #print (data)
-            current_tweet = json.loads(data)
-            created = current_tweet['created_at']
-            if "retweeted_status" in tweet:
-            #print(line['retweeted_status']["retweet_count"])
-                current_RT = current_tweet['retweeted_status']['retweet_count']
-            else:
-                current_RT=0
-            current_tweet_dict={"created_at":created[0:created.find("+")-1],
-                                "user_name":current_tweet['user']['screen_name'],
-                                "profile_pic":current_tweet['user']['profile_image_url'],
-                                "text":current_tweet['text'],
-                                "favourites":current_tweet['user']['favourites_count'],
-                                "RT":current_RT,
-                                "followers":current_tweet['user']['followers_count']}
-
-            new_df = pd.DataFrame([current_tweet_dict])
-            df.append(new_df)
-            
-            return True
-        else:
-            return False
-
-    def on_error(self, status):
-        print (status)
 
 
-while (True):
-
-    #df.to_csv("out.csv")
-
-    #Start sorting and generating database
-    df.sort_values(['RT'],ascending=False)
-
-    df['z'] = df.apply(lambda row: row.RT + row.favourites, axis=1)
-
-    sorted=df.sort_values(["z"],ascending=False)
-    
-
-        
-    #Collect top 5 trending tweets and write to output.json
-    top_5_tweets = sorted[0:5]
-
-    top_5_tweets = top_5_tweets.reset_index()
-    a = top_5_tweets.to_json()
-
-    output = "json="+a
-
-    print("Updated Output.json")
-
-    with open("output.json","w") as output_file:
-        output_file.write(output)
-
-    
-        
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['bitcoin'])
 
 
+
+
+#Collect top 5 trending tweets and write to output.json
+top_5_tweets = sorted[0:5]
+
+top_5_tweets = top_5_tweets.reset_index()
+a = top_5_tweets.to_json()
+
+output = "json="+a
+
+with open("output.json","w") as output_file:
+    output_file.write(output)
 
 
 
@@ -137,7 +87,6 @@ while (True):
 
 
     
-
 
 
 
